3b5ac545c08870f3138d4174a0e85ff262ff15c9
echo.active@gmail.com <echo.active@gmail.com@14e20dfe-5ad8-14dc-7a65-ba347bbb44ac>
2013-05-26 20:36:24 +0000

    Update untuk Import presensi....

--- application/config/database.php
+++ application/config/database.php
@@ -54,7 +54,7 @@ $db['default']['password'] = '';
 $db['default']['database'] = 'dbympi';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
-$db['default']['pconnect'] = TRUE;
+$db['default']['pconnect'] = FALSE;
 $db['default']['db_debug'] = TRUE;
 $db['default']['cache_on'] = FALSE;
 $db['default']['cachedir'] = '';
@@ -63,6 +63,22 @@ $db['default']['dbcollat'] = 'utf8_general_ci';
 $db['default']['swap_pre'] = '';
 $db['default']['autoinit'] = TRUE;
 $db['default']['stricton'] = FALSE;
+
+$db['mybase']['hostname'] = 'localhost';
+$db['mybase']['username'] = 'ekojs';
+$db['mybase']['password'] = '';
+$db['mybase']['database'] = 'mybase';
+$db['mybase']['dbdriver'] = 'mysql';
+$db['mybase']['dbprefix'] = '';
+$db['mybase']['pconnect'] = FALSE;
+$db['mybase']['db_debug'] = TRUE;
+$db['mybase']['cache_on'] = FALSE;
+$db['mybase']['cachedir'] = '';
+$db['mybase']['char_set'] = 'utf8';
+$db['mybase']['dbcollat'] = 'utf8_general_ci';
+$db['mybase']['swap_pre'] = '';
+$db['mybase']['autoinit'] = TRUE;
+$db['mybase']['stricton'] = FALSE;
 
 
 /* End of file database.php */
